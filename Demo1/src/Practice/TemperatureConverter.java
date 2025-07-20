f7991a479ca9c07725f56567138498e1e35d794f
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-20 09:26:31 +0530

    java

---
+++ Demo1/src/Practice/TemperatureConverter.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class TemperatureConverter {
+
+        public static void main(String[] args) {
+            double celsius = 36.5;
+            double fahrenheit = (celsius * 9/5) + 32;
+
+            System.out.println(celsius + "°C = " + fahrenheit + "°F");
+        }
+    }
+
