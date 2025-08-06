80cf2d4aa472b75271be8301a9e243054d1e7ef3
kotireddy10 <kotireddykummita10@gmail.com>
2025-08-06 18:06:56 +0530

    java

---
+++ CoinChange.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class CoinChange {
+    public static int minCoins(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int c : coins) {
+                if (i - c >= 0)
+                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println("Min coins: " + minCoins(coins, amount));
+    }
+}
