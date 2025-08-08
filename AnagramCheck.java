f8e007f24791274ddfcb31eb734b7acad50de592
kotireddy10 <kotireddykummita10@gmail.com>
2025-08-08 11:15:14 +0530

    java

---
+++ AnagramCheck.java
@@ -0,0 +1,17 @@
+// Anagram Checker
+
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static boolean isAnagram(String s1, String s2) {
+        char[] a = s1.replaceAll("\\s", "").toLowerCase().toCharArray();
+        char[] b = s2.replaceAll("\\s", "").toLowerCase().toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("Listen", "Silent")); // true
+    }
+}
