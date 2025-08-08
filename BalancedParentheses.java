ddfaa7e4da1ba60341abf63865b6cbca0a2b2e69
kotireddy10 <kotireddykummita10@gmail.com>
2025-08-08 11:35:00 +0530

    java

---
+++ BalancedParentheses.java
@@ -0,0 +1,21 @@
+//Check Balanced Parentheses Using Stack
+
+import java.util.*;
+
+public class BalancedParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "{[()]}";
+        System.out.println("Is Balanced: " + isValid(s));
+    }
+}
